3a24fb0b6224ec3f5b45f51ce9a8400c6573d7cf
krebdoe <shashank.singh.2@mnsu.edu>
2025-01-30 15:10:07 -0600

    x

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Written By Shashank
+// 1/15/2025
+
+namespace ProjectSpring2025B
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> aListofStudents = new List<Student>();
+            Student aStudent1 = new Student();
+            Student aStudent2 = new Student();
+            Student aStudent3 = new Student();
+            Student aStudent4 = new Student(4, "Gail", "Smith", "Elizabeth", "Nursing");
+            Student aStudent5 = new Student(5, "Rogers", "Evans", "George", "Dental Hygiene");
+
+
+            aStudent1.StudentId = 1;
+            aStudent1.FirstName = "Anna";
+            aStudent1.LastName = "Romanov";
+            aStudent1.MiddleName = "Marie";
+            aStudent1.Major = "Biology";
+
+            aStudent2.StudentId = 2;
+            aStudent2.FirstName = "Robert";
+            aStudent2.LastName = "Junior";
+            aStudent2.MiddleName = "Downey";
+            aStudent2.Major = "Nursing";
+
+            aStudent3.StudentId = 3;
+            aStudent3.FirstName = "Aditya";
+            aStudent3.LastName = "Maharjan";
+            aStudent3.MiddleName = "Dev";
+            aStudent3.Major = "Physical Education";
+
+            aListofStudents.Add(aStudent1);
+            aListofStudents.Add(aStudent2);
+            aListofStudents.Add(aStudent3);
+            aListofStudents.Add(aStudent4);
+            aListofStudents.Add(aStudent5);
+
+            /*
+            Console.WriteLine(aStudent1.ToString());
+            Console.WriteLine(aStudent2.ToString());
+            Console.WriteLine(aStudent3.ToString());
+            Console.WriteLine(aStudent4.ToString());
+            Console.WriteLine(aStudent5.ToString());
+            */
+
+            //This is noe the preferred code
+            foreach(Student aStudent in aListofStudents)
+            {
+                Console.WriteLine(aStudent.ToString());
+            }
+
+            //This is the old format that you may see online
+            foreach (var s in aListofStudents)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,117 @@
+﻿// Written By Shashank
+// 1/15/2025
+
+using System;
+using System.Collections.Generic;
+
+namespace ProjectSpring2025B
+{
+    public class Student
+    {
+        // class variables and they are in camel case
+        private int studentId = -1;
+        private string firstName = "n/a";
+        private string middleName = "n/a";
+        private string lastName = "n/a";
+        private string major = "n/a";
+        // GPA is a calculated field so later in the semester
+        //private double gpa = 0.0;
+
+        // get and sets Accessor Methods = gets and Mutator Methods = sets
+
+        public int StudentId
+        {
+            // I am making this read only just for fun, just for now
+            get { return this.studentId; }
+            //set { this.middleName = value; }
+        }
+        public string FirstName
+        {
+            // This is a very bare bones get and set
+            // there could be a lot of code here
+            // but this is week 0 so there isn't a lot of code here yet
+            // the code would be for checking ranges and such
+
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+        public string MiddleName
+        {
+
+            get { return this.middleName; }
+            set { this.middleName = value; }
+        }
+        public string Major
+        {
+
+            get { return this.major; }
+            set { this.major = value; }
+
+
+        }
+
+        //Constructor
+
+        //When you have two methods with the same name but different parameters
+        //It is called overloading
+        public Student():this(-1, "n/a", "n/a", "n/a", "n/a")
+        {
+            //This is an empty constructor
+            //It is empty because it has no parameters
+            //It may have significant amount of code
+            //overriding vs overloading 
+        }
+        //Constructors
+
+        public Student(int aStudentId, string aFirstName, string aLastName, string aMiddleName, "n/a"):
+            this(aStudentId, aFirstName, aLastName, aMiddleName, "undecided")
+        {
+            //This is a constructor with 4 parameters
+            //Rule of Thumb, use the sets -- that is the capitalized version that will call the set
+            //Try not to ever use the private variables unless you need to 
+            
+            //We no longer need this code because it has no parameters
+            //this.StudentID = aStudentId;
+            //this.FirstName = aFirstName;
+            //this.LastName = aLastName;
+            //this.MiddleName = aMiddleName;
+            //this.Major = "undecided";
+
+
+        //Constructors
+
+        public Student(int aStudentId, string aFirstName, string aLastName, string aMiddleName, string aMajor);
+        {
+            //This is called the full constructor , it has all of the properties
+            //this is not quite industry standard 
+            //that is, not everyone would know the term full constructor
+            this.StudentID = aStudentId;
+            this.FirstName = aFirstName;
+            this.LastName = aLastName;
+            this.MiddleName = aMiddleName;
+            this.Major = aMajor;
+
+        // methods
+
+        public override string ToString()
+        {
+            string message = "";
+            message = message + "Student Id: "      + this.StudentId    + "\n";
+            message = message + "First Name: "      + this.FirstName    + "\n";
+            message = message + "Last Name: "       + this.LastName     + "\n";
+            message = message + "Middle Name: "     + this.MiddleName   + "\n";
+            message = message + "Major: "           + this.Major        + "\n";
+            return message;
+        }
+
+
+
+    }
+}
